a46cc02e79f62bb95123f1dd861d8baed5763852
BrayanStevenRamirezDiaz <brayan.ramirez@claro.com.co>
2024-08-22 16:22:22 -0500

    Cambios hechos en la logica by Brayan

--- codigo.py
+++ codigo.py
@@ -5,99 +5,46 @@ Autor Cristian Moyano
 Contacto cmoyano@cesde.net
 """
 
-print("Este programa compara tres numeros")
+print("Este programa compara cinco numeros")
 
-Numero_1 = 0
-Numero_2 = 0
-Numero_3 = 0
-Numero_4 = 0
-Numero_5 = 0
-
+# Solicitar los cinco números al usuario
 Numero_1 = int(input("Ingrese el primer numero: "))
-
 Numero_2 = int(input("Ingrese el segundo numero: "))
-
 Numero_3 = int(input("Ingrese el tercer numero: "))
+Numero_4 = int(input("Ingrese el cuarto numero: "))
+Numero_5 = int(input("Ingrese el quinto numero: "))
 
-Numero_4 = int(input("Ingrese el tercer numero: "))
-
-Numero_5 = int(input("Ingrese el tercer numero: "))
-
+# Comparaciones entre el cuarto y el quinto número
 if Numero_4 > Numero_5:
-    print("El numero es Mayor")
+    print("El cuarto numero es mayor que el quinto")
+elif Numero_4 < Numero_5:
+    print("El cuarto numero es menor que el quinto")
+else:
+    print("El cuarto numero es igual al quinto")
 
-else: print("No Hay comparacion")     
+# Encontrar el mayor y menor de los primeros tres números
+mayor = max(Numero_1, Numero_2, Numero_3)
+menor = min(Numero_1, Numero_2, Numero_3)
 
-if Numero_1 > Numero_2 and Numero_1 > Numero_3:
-    print("El numero uno es mayor: ",Numero_1)
+print(f"El numero mayor entre los primeros tres es: {mayor}")
+print(f"El numero menor entre los primeros tres es: {menor}")
 
-elif Numero_2 > Numero_1 and Numero_2 > Numero_3:
-    print("El numero dos es mayor: ",Numero_2)
+# Comparaciones de igualdad
+iguales = set([Numero_1, Numero_2, Numero_3, Numero_4, Numero_5])
+if len(iguales) == 1:
+    print("Todos los números son iguales")
+else:
+    if len(set([Numero_1, Numero_2])) < 5:
+        print("El numero uno y el numero dos son iguales" if Numero_1 == Numero_2 else "", end="")
+    if len(set([Numero_1, Numero_3])) < 5:
+        print("El numero uno y el numero tres son iguales" if Numero_1 == Numero_3 else "", end="")
+    if len(set([Numero_2, Numero_3])) < 5:
+        print("El numero dos y el numero tres son iguales" if Numero_2 == Numero_3 else "", end="")
+    if len(set([Numero_4, Numero_5])) < 5:
+        print("El numero cuatro y el numero cinco son iguales" if Numero_4 == Numero_5 else "")
 
-elif Numero_3 > Numero_1 and Numero_3 > Numero_2:
-    print("El numero tres es mayor: ",Numero_3)
+# Comparaciones adicionales para igualdad y mayor o menor
+if (Numero_1 == Numero_2 == Numero_3) or (Numero_2 == Numero_3 == Numero_4) or (Numero_1 == Numero_3 == Numero_4):
+    print("Hay dos o tres números iguales y al menos uno es mayor o menor que los otros")
 
-if Numero_3 > Numero_1 and Numero_4 > Numero_5:
-    print("El numero uno es mayor: ",Numero_1)
-
-elif Numero_1 > Numero_4 and Numero_3 > Numero_5:
-    print("El numero dos es mayor: ",Numero_2)
-
-elif Numero_3 > Numero_1 and Numero_3 > Numero_2:
-    print("El numero tres es mayor: ",Numero_3)
-
-if Numero_1 < Numero_2 and Numero_1 < Numero_3:
-    print("El numero uno es menor: ",Numero_1)
-
-elif Numero_2 < Numero_1 and Numero_2 < Numero_3:
-    print("El numero dos es menor: ",Numero_2)
-
-elif Numero_3 < Numero_1 and Numero_3 < Numero_2:
-    print("El numero tres es menor: ",Numero_3)
-
-
-if Numero_1 == Numero_2 and Numero_1 == Numero_3 and Numero_4 == Numero_5:
-    print("los cinco numeros son iguales: ")
-
-elif Numero_2 == Numero_3:
-    print("El numero dos y el numero tres son iguales: ")
-
-elif Numero_1 == Numero_2:
-    print("El numero uno y el numero dos son iguales: ")
-
-elif Numero_3 == Numero_1:
-    print("El numero tres y el numero uno son iguales")
-
-elif Numero_4 == Numero_5:
-    print("El numero cuatro y el numero cinco son iguales")    
-
-else: print("No hay numeros iguales")
-
-
-
-
-if (Numero_1 == Numero_2)  and Numero_1 == Numero_2 > Numero_3:
-    print("El numero uno y el numero dos son iguales y mayores que numero tres")
-
-elif (Numero_1 == Numero_3) and Numero_1 == Numero_3 > Numero_2:
-    print("El numero uno y el numero tres son iguales y mayores que numero dos")
-
-elif (Numero_2 == Numero_3) and Numero_2 == Numero_3 > Numero_1:
-    print("El numero dos y el numero tres son iguales y mayores que numero uno")
-
-#else: print("No hay numeros mayores e iguales")
-
-
-
-
-if (Numero_1 == Numero_2) and Numero_1 == Numero_2 < Numero_3:
-    print("El numero uno y el numero dos son iguales y menores que numero tres")
-
-elif (Numero_2 == Numero_3) and Numero_2 == Numero_3 < Numero_1:
-    print("El numero dos y el numero tres son iguales y menores que numero uno")
-
-elif (Numero_3 == Numero_1) and Numero_3 == Numero_1 < Numero_2:
-    print("El numero uno y el numero tres son iguales y menores que numero dos")
-
-#else:  print("No hay numero menores e iguales")
  
